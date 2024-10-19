109699ae67f14f4d0190415d649a2f1eef92d12b
Sekhar Gopisetty <sekhar@spinutech.com>
2024-10-19 18:08:06 -0500

    init

---
+++ Receive/Receive.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+const string queueName = "hello";
+
+channel.QueueDeclare(queue: queueName, exclusive: false, autoDelete: false);
+System.Console.WriteLine(" [*] Waiting for messages..");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received   +=  (model,ea) =>
+{
+    var body = ea.Body.ToArray(); 
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+
+channel.BasicConsume(queue:queueName, autoAck: true, consumer: consumer);
+Console.ReadLine();
---
+++ Send/Send.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory{ HostName="localhost"};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+const string queueName = "hello";
+channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete:false, arguments: null);
+
+string message = $"Hello, World at {DateTime.Now.ToLongTimeString()}!";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: string.Empty, 
+                    routingKey: queueName, 
+                    basicProperties: null, 
+                    body: body
+);
